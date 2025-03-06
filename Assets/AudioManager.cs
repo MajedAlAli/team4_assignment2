d024f02bf6c2aa1a1d2f76e147b6eec2d16da556
mohd2k5 <mohd.abdularwani@gmail.com>
2025-03-06 20:03:31 +0400

    updating Audio Manager

---
+++ Assets/AudioManager.cs
@@ -0,0 +1,19 @@
+using UnityEngine.Audio;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public Sound[] sounds;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
