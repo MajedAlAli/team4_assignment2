0707793e36f21d8cbc495e2bb9f81918b170df41
MajedAlAli <alali.majed@outlook.com>
2025-03-09 22:02:07 +0400

    adding script and types to powerups

---
+++ Assets/Challenge 4/Scripts/PowerupX.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public class PowerupX : MonoBehaviour
+{
+    public enum PowerupType { Speed, Strength, Slowdown, Magnet, Jump, Freeze } // Define powerup types
+    public PowerupType powerupType; // Assign type in Inspector
+}
+
